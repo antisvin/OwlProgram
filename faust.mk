a6b19a20cb06975d40f8bfe1cad256cc9d5ed7c0
antisvin <antisvin@gmail.com>
2021-01-25 22:09:54 +0300

    Updates for v22 firmware

--- faust.mk
+++ faust.mk
@@ -4,9 +4,9 @@ FAUSTFLAGS ?= -light
 faust: $(BUILD)/Source/FaustPatch.hpp
 
 $(BUILD)/Source/FaustPatch.hpp: $(PATCHSOURCE)/$(FAUST).dsp
-	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -mem -a FaustCode/owl.cpp -fm faustmath.h $< $(BUILDROOT)/FaustCode/owl.lib -o $@
+	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -mem -a FaustCode/owl.cpp -fm faustmath.h $< $(BUILDROOT)/FaustCode/owl.lib -dlt 1024 -o $@
 	@cp FaustCode/owl.h $(PATCHSOURCE)
 
 $(BUILD)/Source/%Patch.hpp: $(PATCHSOURCE)/%.dsp
-	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -mem -a FaustCode/owl.cpp -fm faustmath.h $< -o $@
+	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -mem -a FaustCode/owl.cpp -fm faustmath.h 1024 $< -o $@
 
